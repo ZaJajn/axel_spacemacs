f06eb09217d82ce8cb2325291cf9a80e985c3649
ZaJajn <zajajn@hotmail.com>
2018-06-08 05:27:03 +0800

    sh install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 cd ~
 sudo dnf install emacs python-pip
-sudo pip3 install virtualenv virtualenvwrapper
+pip3 install virtualenv virtualenvwrapper
 rm -rf .emacs.d .emacs .spacemacs .spacemacs.d
 mkdir ~/.emacs.d
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 
 cd ~
 git clone https://github.com/ZaJajn/axel_spacemacs
-mv axel_spacemacs/init.el ~/.spacemacs
+cp axel_spacemacs/.spacemacs ~
 cd ~
 rm -rf axel_spacemacs
