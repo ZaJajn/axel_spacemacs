0b60defe11d98ddaeceae926545878f9ac3e1d3d
ZaJajn <33850658+ZaJajn@users.noreply.github.com>
2018-07-06 00:12:19 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 cd ~
-sudo dnf install emacs python-pip adobe-source-code-pro-fonts python-virtualenv
-pip3 install virtualenv virtualenvwrapper
-sudo pip3 install jedi
+#sudo dnf install emacs python-pip adobe-source-code-pro-fonts python-virtualenv
+#pip3 install virtualenv virtualenvwrapper
+#sudo pip3 install jedi
 rm -rf .emacs.d .emacs .spacemacs .spacemacs.d
 mkdir ~/.emacs.d
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+sudo dnf install adobe-source-code-pro-fonts
 
 cd ~
 cp axel_spacemacs/.spacemacs ~
 cd ~
 rm -rf axel_spacemacs
 emacs --insecure&
-echo 郑靖岚最咸湿
+echo 龙徳辉最咸湿
